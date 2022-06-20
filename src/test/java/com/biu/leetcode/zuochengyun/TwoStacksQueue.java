8ac60ba6ef517d56fc98b0e2f461bc0acce2b9a2
longbiu <longbiu@foxmail.com>
2022-06-20 16:57:41 +0800

    add: add TwoStacksQueue

--- src/test/java/com/biu/leetcode/zuochengyun/TwoStacksQueue.java
+++ src/test/java/com/biu/leetcode/zuochengyun/TwoStacksQueue.java
@@ -1,10 +1,46 @@
 package com.biu.leetcode.zuochengyun;
 
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @Class_name TwoStacksQueue
- * @Description 由两个栈组成的队列，支持队列的基本操作（add,poll,peek）
+ * @Description 由两个栈组成的队列，支持队列的基本操作（add,poll(检索并删除队列头部),peek（检索但不删除队列头部））
  * @Author longbiu
  * @Date 2022/6/20 15:10
  **/
 public class TwoStacksQueue {
+    public Stack<Integer> stackPush;
+    public Stack<Integer> stackPop;
+
+    public TwoStacksQueue() {
+        stackPush = new Stack<Integer>();
+        stackPop = new Stack<Integer>();
+    }
+
+    public void add(int pushInt) {
+        stackPush.push(pushInt);
+    }
+
+    public int poll() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException("Queue is empty");
+        } else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.pop();
+    }
+
+    public int peek() {
+        if (stackPop.empty() && stackPush.empty()) {
+            throw new RuntimeException("Queue is empty");
+        } else if (stackPop.empty()) {
+            while (!stackPush.empty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+        return stackPop.peek();
+    }
 }
