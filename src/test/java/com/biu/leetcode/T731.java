1cc03c5aa0ebc075c79fd422ff48a72457894aa5
longbiu <longbiu@foxmail.com>
2022-07-19 16:56:03 +0800

    push code class

---
+++ src/test/java/com/biu/leetcode/T731.java
@@ -0,0 +1,20 @@
+package com.biu.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Class_name T731
+ * @Description https://leetcode.cn/problems/my-calendar-ii/
+ * @Author longbiu
+ * @Date 2022/7/19 15:15
+ **/
+public class T731 {
+    public void MyCalendarTwo() {
+
+    }
+
+    public boolean book(int start, int end) {
+        return true;
+    }
+}
